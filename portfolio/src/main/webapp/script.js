ebbdd4db0ac9b7a1e0f855ea64eb5f3ed6bf2194
Stephanie <galvanstephanie2018@gmail.com>
2020-06-04 20:24:08 +0000

    Renaming

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,35 +12,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
-
-window.addEventListener("load", getDummyData)
-function getDummyData() {
+window.addEventListener("load", addDummyDataToDom)
+function addDummyDataToDOM() {
     fetch('/data').then(response => response.json()).then((dummyData) => {
         const commentsContent = document.getElementById('comments');
-        commentsContent.innerHTML = '';
         dummyData.forEach(function(singleDummyData) {
             commentsContent.appendChild(createListElement(singleDummyData));
         });
-        console.log(dummyData);
-   });
+    });
 }
 
 function createListElement(text) {
     const liElement = document.createElement('li');
-    liElement.innerText = text;
+    var liContent = document.createTextNode(text);
+    liElement.appendChild(liContent);
     return liElement;
 }
